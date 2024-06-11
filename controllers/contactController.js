3717d01fdd31b364e07d6229f3efb78c8a2444fd
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2024-06-11 12:58:44 +0700

    Express-Throw Error

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -10,6 +10,11 @@ export const getContacts = (req, res) => {
 // @access public
 export const createContact = (req, res) => {
   console.log("The request body is:  ", req.body)
+  const {name, email, phone} = req.body;
+  if( !name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields art mandatory");
+  }
   res.status(200).json({ message: "Create contact" });
 };
 
