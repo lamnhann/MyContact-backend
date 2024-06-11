16dd8788ec47c2bdf75ba9058d91bd6dc6b26673
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2024-06-11 12:56:31 +0700

    Build-in middlewares for post request body

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -9,6 +9,7 @@ export const getContacts = (req, res) => {
 // @route POST /api/contacts
 // @access public
 export const createContact = (req, res) => {
+  console.log("The request body is:  ", req.body)
   res.status(200).json({ message: "Create contact" });
 };
 
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/contacts", contactRouters);
 
 app.listen(port, () => {
