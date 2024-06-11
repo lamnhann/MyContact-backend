c4380fdca1ceed064c6614b58c2b39b5bbcde32e
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2024-06-11 13:41:37 +0700

    Mongodb setup and connect

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import contactRouters from './routes/contactRoutes.js';
 import errorHandler from './middleware/errorHandler.js';
@@ -7,7 +8,15 @@ dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO)
+    .then(()=> {
+        console.log('MongoDb connected');
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB')
+    });
+
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use("/api/contacts", contactRouters);
